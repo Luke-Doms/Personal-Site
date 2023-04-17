cccfbe52e9417f810901fed97e39581bbaeab6cd
Luke <90083045+Luke-Doms@users.noreply.github.com>
2023-04-17 11:57:47 -0400

    deploy site

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -23,6 +23,7 @@ const SocialLinks = () => {
                 </>
             ), 
             href: 'https://github.com/Luke-Doms', 
+            style: ""
         }, 
         {
             id: 3, 
@@ -40,8 +41,8 @@ const SocialLinks = () => {
     <div className=" hidden lg:flex flex-col top-[35%] left-0 fixed">
         <ul>
             {links.map(({id, child, href, style, download}) => (
-                <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300" + " " + style}>
-                    <a href={href} className="flex justify-between items-center w-full text-white" download={download} target="_blank"> 
+                <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 ${style}`}>
+                    <a href={href} rel="noopener noreferrer" className="flex justify-between items-center w-full text-white" download={download} target="_blank"> 
                         {child}
                     </a>
                 </li>
